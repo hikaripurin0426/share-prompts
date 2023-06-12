0781d14ec75b5348447b7a5690368556f37a7e0c
Hikari <sunheavenlife@gmail.com>
2023-06-12 16:01:23 +1100

    change title

--- app/page.jsx
+++ app/page.jsx
@@ -3,13 +3,13 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
-            Prompt Master
+            Prompts Master
             <br className="max-md:hidden" />
             <span className="orange_gradient text-center">Let's share your AI prompts!</span>
         </h1>
         <p className="desc text-center">
-            Prompt MasterはChat-GPTなどのAIに渡す命令文（プロンプト）を共有するためのプラットフォームです。<br />
-            みんなが発見した便利なプロンプトをシェアして、最新AIを使いこなしましょう！
+            Prompts MasterはChat-GPTなどのAIに渡す命令文（プロンプト）を共有するためのプラットフォームです。<br />
+            みんなが発見した便利なプロンプトをシェアして、最新AIを使いこなそう！
         </p>
         
         <Feed />
